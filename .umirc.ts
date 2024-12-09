d8982698684ea0589e9d886e0b83b102c92de36a
luyi.lss <lushasha@come-future.com>
2024-12-09 19:26:46 +0800

    Updates

---
+++ .umirc.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "umi";
+
+export default defineConfig({
+  routes: [
+    { path: "/", component: "@/pages/edit-resume" },
+  ],
+  theme: { 
+    '@primary-color': '#44a6d7',
+    "@grey-2": '#616161',
+    "@grey-1": "#f5f7f9",
+    "@grey-3": "#0003"
+  },
+  plugins: [
+    '@umijs/plugins/dist/dva',
+  ],
+  dva: {
+    immer: {
+      enableES5: true,
+      enableAllPlugins: true,
+    },
+  },
+  npmClient: 'pnpm',
+  mfsu: {
+    shared: {
+      react: {
+        singleton: true,
+      },
+    },
+  },
+});
